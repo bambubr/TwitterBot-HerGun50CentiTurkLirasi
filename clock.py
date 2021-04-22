2e5c673ecabbcb0fe398e2b8c57f55d542a93721
Jair Angeluci <34008533+JairAngeluci@users.noreply.github.com>
2021-04-22 20:02:56 -0300

    teste clock

--- clock.py
+++ clock.py
@@ -2,8 +2,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler(timezone="america/sao_paulo")
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=20)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=21)
 def scheduled_job():
     print('Postou.')
+    exec(open('TodoDiaO50CentEmReais.py').read())
 
 sched.start()
