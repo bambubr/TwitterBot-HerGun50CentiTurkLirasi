22a00f3412b7c1e13be8027c6f9d4edf35a29ed0
Jair Angeluci <34008533+JairAngeluci@users.noreply.github.com>
2021-04-22 21:03:46 -0300

    .

--- TodoDiaO50CentEmReais.py
+++ TodoDiaO50CentEmReais.py
@@ -3,26 +3,22 @@ from Twitter import Bot
 from CotacaoDolar import Cotacao
 from os import environ
 
-def main():
-    # chaves de acesso da API / usuário do bot
-    consumer_key = environ['CONSUMER_KEY']
-    consumer_secret = environ['CONSUMER_SECRET']
-    access_token = environ['ACCESS_KEY']
-    access_token_secret = environ['ACCESS_SECRET']
+# chaves de acesso da API / usuário do bot
+consumer_key = environ['CONSUMER_KEY']
+consumer_secret = environ['CONSUMER_SECRET']
+access_token = environ['ACCESS_KEY']
+access_token_secret = environ['ACCESS_SECRET']
 
-    # acessar o json com a cotação do dólar e salvar o valor da cotação + momento da atualização
-    cotDolar = Cotacao("https://economia.awesomeapi.com.br/last/USD-BRL")
-    dolar = cotDolar.retornarValorDolar()
-    dataCotacao = cotDolar.retornarData()
+# acessar o json com a cotação do dólar e salvar o valor da cotação + momento da atualização
+cotDolar = Cotacao("https://economia.awesomeapi.com.br/last/USD-BRL")
+dolar = cotDolar.retornarValorDolar()
+dataCotacao = cotDolar.retornarData()
 
-    # criar/sobrescrever a imagem MeioDolar.jpg
-    img = Imagem("R$ " + str(round(dolar/2, 2)))
-    img.CriarImagemAlterada()
+# criar/sobrescrever a imagem MeioDolar.jpg
+img = Imagem("R$ " + str(round(dolar/2, 2)))
+img.CriarImagemAlterada()
 
-    # acessar a api do twitter e publicar a imagem
-    bot = Bot(consumer_key, consumer_secret, access_token, access_token_secret)
-    api = bot.authenticate()
-    bot.publicar(api, str(round(dolar, 2)), dataCotacao)
-
-if __name__ == '__main__':
-    main()
+# acessar a api do twitter e publicar a imagem
+bot = Bot(consumer_key, consumer_secret, access_token, access_token_secret)
+api = bot.authenticate()
+bot.publicar(api, str(round(dolar, 2)), dataCotacao)
