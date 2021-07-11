6f45faa9ecf77c787956c94b30e9e7e24e806a0b
bambubr <Jdomingos@gmail.com>
2021-07-11 20:58:28 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,12 +2,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler(timezone="Europe/Istanbul")
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=10)
 def scheduled_job():
     print('Exec 1000...')
     exec(open('HerGun50CentiTurkLirasi.py').read())
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=18)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18)
 def scheduled_job():
     print('Exec 1800...')
     exec(open('HerGun50CentiTurkLirasi.py').read())
